834cfb01973fbaf1493314fe2acfb81eaf747cd1
Fernando Ruiloba <duenderuiloba8@hotmail.com>
2024-09-13 21:12:38 -0400

    Full programming

--- hw2/decision_tree.py
+++ hw2/decision_tree.py
@@ -1,6 +1,5 @@
 import argparse
 from collections import OrderedDict
-import sys
 import numpy as np  # type: ignore
 
 
@@ -22,28 +21,42 @@ class Node:
         self.vote = None
         self.header = None
         self.mutual_info = None
-
-def print_tree(Node):
-    pass
-
+        self.stats_zeros = None
+        self.stats_ones = None
 
 def load_file_contents(input_file_name: str):
     print(f"Loading input file: {input_file_name}")
     input_data = np.genfromtxt(
         fname=input_file_name, delimiter="\t", dtype=None, encoding=None
     )
-    input_data_inputs = input_data[1:, 0 : input_data.shape[1] - 2]
+    input_data_inputs = input_data[1:, 0 : input_data.shape[1] - 1]
     input_data_outputs = input_data[1:, input_data.shape[1] - 1]
-    input_data_headers = input_data[0,:]
+    input_data_headers = input_data[0, 0 : input_data.shape[1] - 1]
+    labels_header = input_data[0, input_data.shape[1] - 1]
 
-    return (input_data_inputs, input_data_outputs, input_data_headers)
+    return (input_data_inputs, input_data_outputs, input_data_headers, labels_header)
 
+def get_proportions(inputs):
+    values, counts = np.unique(inputs, return_counts=True)
+
+    if not counts.any():
+        zero_proportion = 0
+        one_proportion = 0
+    elif counts.size == 1:
+        if values[0] == "0":
+            zero_proportion = counts[0] / inputs.size
+            one_proportion = 0
+        else:
+            zero_proportion = 0
+            one_proportion = counts[0] / inputs.size
+    else:
+        zero_proportion = counts[0] / inputs.size
+        one_proportion = counts[1] / inputs.size
+
+    return (zero_proportion, one_proportion)
 
 def entropy(labels):
-    _, counts = np.unique(labels, return_counts=True)
-
-    zero_proportion = counts[0] / labels.size if counts.any() else 0
-    one_proportion = counts[1] / labels.size if counts.any() and counts.size > 1 else 0
+    zero_proportion, one_proportion = get_proportions(labels)
 
     return -1 * np.sum(
         [
@@ -54,6 +67,7 @@ def entropy(labels):
 
 def split(col_to_split, inputs):
     col_to_split_bools = col_to_split.astype(bool)
+
     inputs_when_zero = inputs[np.invert(col_to_split_bools)]
     inputs_when_one = inputs[col_to_split_bools]
 
@@ -63,17 +77,13 @@ def split(col_to_split, inputs):
 # but at least it doesn't grow exponentially. Plus no loops!
 def mutual_information(inputs_col, labels):
     entropy_labels = entropy(labels)
-    _, counts = np.unique(inputs_col, return_counts=True)
-
-    fraction_zeros = counts[0] / inputs_col.size if counts.any() else 0
-    fraction_ones = counts[1] / inputs_col.size if counts.any() and counts.size > 1 else  0
-
+    zero_proportion, one_proportion = get_proportions(inputs_col)
     labels_for_zero_input, labels_for_one_input = split(inputs_col, labels)
 
     return entropy_labels - np.sum(
         [
-            fraction_zeros * entropy(labels_for_zero_input),
-            fraction_ones * entropy(labels_for_one_input),
+            zero_proportion * entropy(labels_for_zero_input),
+            one_proportion * entropy(labels_for_one_input),
         ]
     )
 
@@ -85,6 +95,22 @@ def majority_vote(labels):
         return values[0]
     else:
         return values[0] if counts[0] > counts[1] else values[1]
+    
+def stats(labels):
+    values, counts = np.unique(labels, return_counts=True)
+    if not counts.any():
+        return f"[0 0/0 1]"
+    elif counts.size == 1:
+        if values[0] == "0":
+            return f"[{counts[0]} 0/0 1]"
+        else:
+            return f"[0 0/{counts[0]} 1]"
+    else:
+        return f"[{counts[0]} 0/{counts[1]} 1]"
+    
+def are_all_inputs_the_same(inputs):
+    _, counts = np.unique(inputs, return_counts=True)
+    return (not counts.any()) or counts[0] == 0 or inputs.size == counts[0]
 
 def build_tree(inputs, labels, headers, depth, max_depth):
 
@@ -92,38 +118,79 @@ def build_tree(inputs, labels, headers, depth, max_depth):
     node.attr = inputs
     node.vote = majority_vote(labels)
 
-    if (depth >= max_depth or inputs.shape[1] <= 1):
+    if (depth >= max_depth or inputs.shape[1] < 1 or are_all_inputs_the_same(labels) or are_all_inputs_the_same(inputs)):
         return node
 
     mutual_informations = {}
     for col_index in range(inputs.shape[1]):
-         mutual_informations[col_index] = mutual_information(inputs[:, col_index], labels)
+        mutual_informations[col_index] = mutual_information(inputs[:, col_index], labels)
 
     # https://www.geeksforgeeks.org/python-sort-python-dictionaries-by-key-or-value/
-    sorted_mutual_infos = OrderedDict({key:value for key, value in sorted(mutual_informations.items(), key=lambda mutual_informations: mutual_informations[1])})
+    # tie breaker: https://stackoverflow.com/questions/54300715/python-3-list-sorting-with-a-tie-breaker
+    sorted_mutual_infos = OrderedDict(
+        {
+            key:value for key, value in sorted(
+                mutual_informations.items(),
+                key=lambda mutual_informations: (mutual_informations[1], -mutual_informations[0]))
+        })
 
-    # print("mutual_infos", sorted_mutual_infos)
+    col_to_split_index, col_mutual_info = sorted_mutual_infos.popitem()
 
-    col_to_split = sorted_mutual_infos.popitem()
-    node.mutual_info = col_to_split[1]
-    node.header = headers[col_to_split[0]]
-    new_headers = np.delete(headers, col_to_split[0], 0)
+    node.mutual_info = col_mutual_info
+    node.header = headers[col_to_split_index]
+    new_headers = np.delete(headers, col_to_split_index, 0)
 
-    # print(f"splitting by column {headers[col_to_split[0]]} with MI {col_to_split[1]}")
+    col_to_split = inputs[:,col_to_split_index]
+    new_inputs = np.delete(inputs, col_to_split_index, 1)
+    inputs_with_zero, inputs_with_one = split(col_to_split, new_inputs)
+    labels_with_zero, labels_with_one = split(col_to_split, labels)
+    node.stats_zeros = stats(labels_with_zero)
+    node.stats_ones = stats(labels_with_one)
 
-    inputs_with_zero, inputs_with_one = split(inputs[:,col_to_split[0]], inputs)
-    labels_with_zero, labels_with_one = split(inputs[:,col_to_split[0]], labels)
-
-    # print("inputs shape", inputs.shape)
-    # print("inputs with zero shape", inputs_with_zero.shape)
-    # print("labels with zero shape", labels_with_zero.shape)
-    # print("inputs with one shape", inputs_with_one.shape)
-    # print("labels with one shape", labels_with_one.shape)
-
-    node.right = build_tree(inputs_with_zero, labels_with_zero, new_headers, depth + 1, max_depth)
-    node.left = build_tree(inputs_with_one, labels_with_one, new_headers, depth + 1, max_depth)
+    node.left = build_tree(inputs_with_zero, labels_with_zero, new_headers, depth + 1, max_depth)
+    node.right = build_tree(inputs_with_one, labels_with_one, new_headers, depth + 1, max_depth)
 
     return node
+
+def predict_row_label(node: Node, inputs, headers):
+
+        if (node.left == None and node.right == None):
+            return node.vote
+
+        # find the item that the node is talking about in the inputs
+        input_index = np.where(headers == node.header)[0]
+        if (inputs[input_index] == "0"):
+            return predict_row_label(node.left, inputs, headers)
+        else:
+            return predict_row_label(node.right, inputs, headers)
+        
+def predict(node, inputs, headers):
+    predicted_labels = []
+    for row in inputs:
+        predicted_labels = np.append(predicted_labels, predict_row_label(node, row, headers))
+
+    return predicted_labels
+
+def get_error_ratio(predicted_outputs, real_outputs):
+    errorCount = 0
+
+    for i in range(predicted_outputs.size):
+        if predicted_outputs[i] != None and int(predicted_outputs[i]) != int(real_outputs[i]):
+            errorCount += 1
+
+    return errorCount / predicted_outputs.size
+
+def print_tree_inner(node, depth = 0):
+    if node.left == None and node.right == None:
+        return ""
+    
+    depth_string = "| " * (depth + 1)
+
+    return (f"\n{depth_string} {node.header} = 0: {node.stats_zeros}" + print_tree_inner(node.left, depth + 1)
+            + f"\n{depth_string} {node.header} = 1: {node.stats_ones}" + print_tree_inner(node.right, depth + 1))
+
+def print_tree(node, labels):
+    return stats(labels) + print_tree_inner(node)
 
 if __name__ == "__main__":
     # This takes care of command line argument parsing for you!
@@ -159,12 +226,39 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    # Here's an example of how to use argparse
-    print_out = args.print_out
+    def print_to_file(print_out, content):
+        print(f"Writing to out file {print_out}")
+        with open(print_out, "w") as txt_file:
+            for line in content:
+                txt_file.write(str(line) + "\n")
 
-    train_inputs, train_labels, headers = load_file_contents(args.train_input)
-    head_node = build_tree(train_inputs, train_labels, headers, 0, 3)
+    def print_tree_to_file(print_out, content):
+        print(f"Writing to out file {print_out}")
+        with open(print_out, "w") as txt_file:
+            txt_file.write(str(content) + "\n")
 
-    # Here is a recommended way to print the tree to a file
-    # with open(print_out, "w") as file:
-    #     print_tree(dTree, file)
+    def print_metrics_to_file(print_out, train_error, test_error):
+        print(f"Writing to out file {print_out}")
+        with open(print_out, "w") as txt_file:
+            txt_file.write(f'error(train): {train_error}\n')
+            txt_file.write(f'error(test): {test_error}\n')
+
+    train_inputs, train_labels, train_headers, labels_header = load_file_contents(args.train_input)
+    # print("train_inputs.shape", train_inputs.shape, "train_labels.shape", train_labels.shape, "train_headers.shape", train_headers.shape, "labels_header", labels_header)
+
+    head_node = build_tree(train_inputs, train_labels, train_headers, 0, args.max_depth)
+
+    train_predicted_labels = predict(head_node, train_inputs, train_headers)
+    print_to_file(args.train_out, train_predicted_labels)
+    
+    test_inputs, test_labels, test_headers, test_labels_header = load_file_contents(args.test_input)
+    test_predicted_labels = predict(head_node, test_inputs, test_headers)
+    print_to_file(args.test_out, test_predicted_labels)
+
+    train_error_ratio = get_error_ratio(train_predicted_labels, train_labels)
+    test_error_ratio = get_error_ratio(test_predicted_labels, test_labels)
+    print_metrics_to_file(args.metrics_out, train_error_ratio, test_error_ratio)
+
+    printed_tree = print_tree(head_node, train_labels)
+    print_tree_to_file(args.print_out, printed_tree)
+    print(printed_tree)
