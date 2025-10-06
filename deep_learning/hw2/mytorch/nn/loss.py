ccc654e1a53003b16ca90505fcd155094d11b239
Fernando Ruiloba <feruiloba@gmail.com>
2025-10-06 13:46:13 -0600

    Add mytorch functions from previous HW

--- deep_learning/hw2/mytorch/nn/loss.py
+++ deep_learning/hw2/mytorch/nn/loss.py
@@ -1,41 +1,72 @@
 import numpy as np
+from mytorch.nn.activation import Softmax
 
-# Copy your Linear class from HW1P1 here
 class MSELoss:
     def forward(self, A, Y):
+        """
+        Calculate the Mean Squared error (MSE)
+        :param A: Output of the model of shape (N, C)
+        :param Y: Ground-truth values of shape (N, C)
+        :Return: MSE Loss (scalar)
+
+        Read the writeup (Hint: MSE Loss Section) for implementation details for below code snippet.
+        """
         self.A = A
         self.Y = Y
-        N = A.shape[0]
-        C = A.shape[1]
-        se = None  # TODO
-        sse = None  # TODO
-        mse = sse / (N * C)
-
-        return NotImplemented
+        self.N = A.shape[0]
+        self.C = A.shape[1]
+        se = (A - Y) ** 2
+        sse = np.ones(self.N) @ se @ np.ones(self.C)
+        mse = sse / (self.N * self.C)
+        
+        return mse
 
     def backward(self):
-        dLdA = None
-        return NotImplemented
+        """
+        Calculate the gradient of MSE Loss wrt model output A.
+        :Return: Gradient of loss L wrt model output A.
 
+        Read the writeup (Hint: MSE Loss Section) for implementation details for below code snippet.
+        """
+        dLdA = 2 * (self.A - self.Y) / (self.N * self.C)
+
+        return dLdA
 
 class CrossEntropyLoss:
     def forward(self, A, Y):
+        """
+        Calculate the Cross Entropy Loss (XENT)
+        :param A: Output of the model of shape (N, C)
+        :param Y: Ground-truth values of shape (N, C)
+        :Return: CrossEntropyLoss (scalar)
+
+        Read the writeup (Hint: Cross-Entropy Loss Section) for implementation details for below code snippet.
+        Hint: Read the writeup to determine the shapes of all the variables.
+        Note: Use dtype ='f' whenever initializing with np.zeros()
+        """
         self.A = A
         self.Y = Y
-        N = A.shape[0]
-        C = A.shape[1]
-        Ones_C = np.ones((C, 1), dtype="f")
-        Ones_N = np.ones((N, 1), dtype="f")
+        self.N = self.A.shape[0]
+        self.C = self.A.shape[1]
 
-        self.softmax = None  # TODO
-        crossentropy = None  # TODO
-        sum_crossentropy = None  # TODO
-        L = sum_crossentropy / N
+        Ones_C = np.ones(self.C)
+        Ones_N = np.ones(self.N)
 
-        return NotImplemented
+        self.softmax = Softmax() # Can you reuse your own softmax here, if not rewrite the softmax forward logic?
+
+        crossentropy = (-Y * np.log(self.softmax.forward(self.A))) @ Ones_C
+        sum_crossentropy_loss = Ones_N @ crossentropy
+        mean_crossentropy_loss = sum_crossentropy_loss / self.N
+
+        return mean_crossentropy_loss
 
     def backward(self):
+        """
+        Calculate the gradient of Cross-Entropy Loss wrt model output A.
+        :Return: Gradient of loss L wrt model output A.
 
-        dLdA = None  # TODO
-
-        return NotImplemented
+        Read the writeup (Hint: Cross-Entropy Loss Section) for implementation details for below code snippet.
+        """
+        dLdA = (self.softmax.forward(self.A) - self.Y) / self.N
+        
+        return dLdA
