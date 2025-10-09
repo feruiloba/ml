8a2575a987eba52fbf19e772f35b295b35106dd4
Fernando Ruiloba <feruiloba@gmail.com>
2025-10-09 01:35:56 -0400

    Pooling is working

--- deep_learning/hw2/mytorch/nn/pool.py
+++ deep_learning/hw2/mytorch/nn/pool.py
@@ -13,7 +13,21 @@ class MaxPool2d_stride1():
         Return:
             Z (np.array): (batch_size, out_channels, output_width, output_height)
         """
-        raise NotImplementedError
+        self.A = A
+        self.output_height = A.shape[3] - self.kernel + 1
+        self.output_width = A.shape[2] - self.kernel + 1
+
+        Z = np.zeros((A.shape[0], A.shape[1], self.output_height, self.output_width))
+        self.max_indices = np.zeros_like(Z, dtype=int)
+
+        for i in range(self.output_height):
+            for j in range(self.output_width):
+                Z[:, :, i, j] += np.max(A[:, :, i:i+self.kernel, j:j+self.kernel], axis=(2, 3))
+
+                flattened_kernel_segment = A[:, :, i:i+self.kernel, j:j+self.kernel].reshape(A.shape[0], A.shape[1], -1)
+                self.max_indices[:, :, i, j] = np.argmax(flattened_kernel_segment, axis=2)
+
+        return Z
 
     def backward(self, dLdZ):
         """
@@ -22,7 +36,20 @@ class MaxPool2d_stride1():
         Return:
             dLdA (np.array): (batch_size, in_channels, input_width, input_height)
         """
-        raise NotImplementedError
+        #coordinates = np.unravel_index(self.max_indices, self.A.shape)
+        dLdA = np.zeros_like(self.A)
+
+        for i in range(self.output_height):
+            for j in range(self.output_width):
+                indexing_flaten = self.max_indices[:, :, i, j]
+                relative_height, relative_width = np.unravel_index(indexing_flaten, (self.kernel, self.kernel))
+                abs_height = relative_height + i
+                abs_width = relative_width + j
+
+                for b in range(dLdZ.shape[0]):
+                    for c in range(dLdZ.shape[1]):
+                        dLdA[b, c, abs_height[b, c], abs_width[b, c]] += dLdZ[b, c, i, j]
+        return dLdA
 
 
 class MeanPool2d_stride1():
@@ -36,7 +63,15 @@ class MeanPool2d_stride1():
         Return:
             Z (np.array): (batch_size, out_channels, output_width, output_height)
         """
-        raise NotImplementedError
+        self.A = A
+        self.output_height = A.shape[2] - self.kernel + 1
+        self.output_width = A.shape[3] - self.kernel + 1
+        Z = np.zeros((A.shape[0], A.shape[1], self.output_height, self.output_width))
+        for i in range(self.output_height):
+            for j in range(self.output_width):
+                Z[:,:, i, j] = np.mean(A[:, :, i:i+self.kernel, j:j+self.kernel], axis=(2, 3))
+
+        return Z
 
     def backward(self, dLdZ):
         """
@@ -45,8 +80,14 @@ class MeanPool2d_stride1():
         Return:
             dLdA (np.array): (batch_size, in_channels, input_width, input_height)
         """
+        dLdA = np.zeros_like(self.A)
+        for b in range(dLdZ.shape[0]):
+            for c in range(dLdZ.shape[1]):
+                for i in range(dLdZ.shape[2]):
+                    for j in range(dLdZ.shape[3]):
+                        dLdA[b, c, i:i+self.kernel, j:j+self.kernel] += dLdZ[b, c, i, j] / (self.kernel * self.kernel)
 
-        raise NotImplementedError
+        return dLdA
 
 
 class MaxPool2d():
@@ -55,8 +96,8 @@ class MaxPool2d():
         self.stride = stride
 
         # Create an instance of MaxPool2d_stride1
-        self.maxpool2d_stride1 = None  # TODO
-        self.downsample2d = None  # TODO
+        self.maxpool2d_stride1 = MaxPool2d_stride1(kernel)
+        self.downsample2d = Downsample2d(stride)
 
     def forward(self, A):
         """
@@ -66,7 +107,9 @@ class MaxPool2d():
             Z (np.array): (batch_size, out_channels, output_width, output_height)
         """
 
-        raise NotImplementedError
+        pooled_A = self.maxpool2d_stride1.forward(A)
+        Z = self.downsample2d.forward(pooled_A)
+        return Z
 
     def backward(self, dLdZ):
         """
@@ -75,7 +118,9 @@ class MaxPool2d():
         Return:
             dLdA (np.array): (batch_size, in_channels, input_width, input_height)
         """
-        raise NotImplementedError
+        upsampled_dLdZ = self.downsample2d.backward(dLdZ)
+        dLdA = self.maxpool2d_stride1.backward(upsampled_dLdZ)
+        return dLdA
 
 
 class MeanPool2d():
@@ -85,8 +130,8 @@ class MeanPool2d():
         self.stride = stride
 
         # Create an instance of MaxPool2d_stride1
-        self.meanpool2d_stride1 = None  # TODO
-        self.downsample2d = None  # TODO
+        self.meanpool2d_stride1 = MeanPool2d_stride1(kernel)
+        self.downsample2d = Downsample2d(stride)
 
     def forward(self, A):
         """
@@ -95,7 +140,9 @@ class MeanPool2d():
         Return:
             Z (np.array): (batch_size, out_channels, output_width, output_height)
         """
-        raise NotImplementedError
+        pooled_A = self.meanpool2d_stride1.forward(A)
+        Z = self.downsample2d.forward(pooled_A)
+        return Z
 
     def backward(self, dLdZ):
         """
@@ -104,4 +151,6 @@ class MeanPool2d():
         Return:
             dLdA (np.array): (batch_size, in_channels, input_width, input_height)
         """
-        raise NotImplementedError
+        upsampled_dLdZ = self.downsample2d.backward(dLdZ)
+        dLdA = self.meanpool2d_stride1.backward(upsampled_dLdZ)
+        return dLdA
